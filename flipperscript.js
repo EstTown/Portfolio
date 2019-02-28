2b060f305b25276420863168f5b32053b5cf9f06
Frederik Hansen <esttown@Frederiks-MacBook-Pro.local>
2019-02-28 21:39:42 +0100

    button added

--- flipperscript.js
+++ flipperscript.js
@@ -1,4 +1,8 @@
 function flipper() {
 	var card = document.querySelector('#innercontent');
+	var btn = document.querySelector('#btnflipper')
 	card.classList.toggle('flipped');
+	
+	if (btn.innerHTML=="About me") btn.innerHTML = "See Projects";
+    else btn.innerHTML = "About me"
 }
